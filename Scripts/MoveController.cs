848c7f3d93bf475c968cd715ecc210c829a23ef9
notdule <148584497+notdule@users.noreply.github.com>
2024-03-19 23:07:43 +0100

    Removing Unnecessary Comments

--- Scripts/MoveController.cs
+++ Scripts/MoveController.cs
@@ -24,14 +24,12 @@ public class MoveController : MonoBehaviour
     private bool facingRight = true;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
